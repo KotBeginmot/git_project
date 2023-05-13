f1f1b9b97ae0a38c69371138875ca86a58dba046
KotBeginmot <iliynaea@gmail.com>
2023-05-14 01:48:12 +0300

    add sub

--- git_project/src/funcs.py
+++ git_project/src/funcs.py
@@ -2,4 +2,6 @@ def sum_num(x, y):
     print(x + y)
 sum_num(1,2)
 
-
+def sub_num(x, y):
+    print(x - y)
+sub_num(1,2)
--- git_project/src/main.py
+++ git_project/src/main.py
@@ -1,3 +1,4 @@
-from funcs import sum_num
+from funcs import sum_num,sub_num
 
 print(sum_num(1, 2))
+print(sub_num(1, 2))
